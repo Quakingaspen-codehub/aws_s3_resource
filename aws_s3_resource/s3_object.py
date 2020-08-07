536073b93ab2e3e55f0c9e73e5625a9648fbb3ed
fissal <fissal@quakingaspen.net>
2020-08-07 10:03:08 +0300

    Updates:
    1. presigned_url.py added to S3Object class.
    2. presigned_url_example.ipynb added.

--- aws_s3_resource/s3_object.py
+++ aws_s3_resource/s3_object.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 
+import boto3
 from botocore.client import ClientError
 
 from . import S3
@@ -102,3 +103,18 @@ class S3Object(S3):
     @classmethod
     def num_objects(cls, bucket_name):
         return len(cls.list_all(bucket_name, False))
+
+    @staticmethod
+    def generate_presigned_url(bucket_name, object_name, expiration):
+        """Generate a presigned URL to share an S3 object    :param bucket_name: string
+        :param object_name: string
+        :param expiration: Time in seconds for the presigned URL to remain valid
+        :return: Presigned URL as string. If error, returns None.
+        """
+        s3_client = boto3.client('s3')
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+
+        return response
