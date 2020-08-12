055a808bc279d819fc2d9785f32d0d68c59bbfbe
akhanji1964 <44018433+akhanji1964@users.noreply.github.com>
2020-08-12 17:36:39 +0300

    Update __init__.py

--- aws_s3_resource/__init__.py
+++ aws_s3_resource/__init__.py
@@ -4,4 +4,4 @@ import boto3
 
 
 class S3:
-    resource = boto3.resource('s3', region_name=os.environ['region_name'])
+    resource = boto3.resource('s3', region_name=os.environ['AWS_REGION_NAME'])
